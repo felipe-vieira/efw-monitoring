cbe5c8ca9766247cbf336cf0aeb7535945c8c965
Wagner Spirigoni <wspirigoni@sp.r7.com.br>
2012-08-14 09:42:22 -0300

    Adicionada classe SendEmail

---
+++ ColetaAgente/src/br/com/fiap/coleta/util/cgt/SendEmail.java
@@ -0,0 +1,105 @@
+package br.com.fiap.coleta.util.cgt;
+
+import java.util.*;
+import javax.activation.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+
+public class SendEmail
+{
+	
+   private String[] to = null;
+   private String from = "";
+   private String subject = "";
+   private String message = "";
+   private String host = "";
+   private String user = "";
+   private String password = "";
+	
+   public void send()
+   {
+      Properties properties = System.getProperties();
+
+      properties.setProperty("mail.smtp.host", this.host);
+      properties.setProperty("mail.user", this.user);
+      properties.setProperty("mail.password", this.password);
+
+      Session session = Session.getDefaultInstance(properties);
+
+      try{
+         MimeMessage message = new MimeMessage(session);
+
+         message.setFrom(new InternetAddress(from));
+    
+         for (int i=0; to.length > i; i++)
+         {
+        	 message.addRecipient(Message.RecipientType.TO,
+                     new InternetAddress(to[i]));
+         }       
+
+         message.setSubject(this.subject);
+
+         message.setText(this.message);
+
+         Transport.send(message);
+      }catch (MessagingException mex) {
+         mex.printStackTrace();
+      }
+   }
+
+public String[] getTo() {
+	return to;
+}
+
+public void setTo(String[] to) {
+	this.to = to;
+}
+
+public String getFrom() {
+	return from;
+}
+
+public void setFrom(String from) {
+	this.from = from;
+}
+
+public String getSubject() {
+	return subject;
+}
+
+public void setSubject(String subject) {
+	this.subject = subject;
+}
+
+public String getMessage() {
+	return message;
+}
+
+public void setMessage(String message) {
+	this.message = message;
+}
+
+public String getHost() {
+	return host;
+}
+
+public void setHost(String host) {
+	this.host = host;
+}
+
+public String getUser() {
+	return user;
+}
+
+public void setUser(String user) {
+	this.user = user;
+}
+
+public String getPassword() {
+	return password;
+}
+
+public void setPassword(String password) {
+	this.password = password;
+}
+}
