f5d37f510d1053ba8ff8d0346e6054b9871ac1b0
felipe-vieira <Felipe@SAMS-XIII>
2012-08-22 21:49:20 -0300

    Correção do sql

--- sql/basedata.sql
+++ sql/basedata.sql
@@ -20,5 +20,5 @@ INSERT INTO tipo_alarme(id,descricao,mensagem,threshold,unidade) VALUES(12,'Thre
 INSERT INTO tipo_alarme(id,descricao,mensagem,threshold,unidade) VALUES(13,'Threshold Memoria BD', 'O uso de memoria do BD atingiu o limite estabelecido',1,'%');
 INSERT INTO tipo_alarme(id,descricao,mensagem,threshold) VALUES(14,'Status do Job','O job ? foi concluido com o status ?.',0);
 INSERT INTO tipo_alarme(id,descricao,mensagem,threshold,unidade) VALUES(15,'Tempo de Backup','O tempo desde o ultimo backup excedeu o tempo limite estabelecido',1,'s');
-INSERT INTO tipo_alarme(id,descricao,mensagem,threshold) VALUES(16,'Status do BD','O status do BD de dados foi alterado para: ?',1,'0');
+INSERT INTO tipo_alarme(id,descricao,mensagem,threshold) VALUES(16,'Status do BD','O status do BD de dados foi alterado para: ?',0);
 INSERT INTO tipo_alarme(id,descricao,mensagem,threshold,unidade) VALUES(17,'Tempo de Execução do Job','O tempo de execução do job ? excedeu o limite estabelecido',1,'s');
