8f1ba149574902521e54d11036e27f99d40e4688
Felipe Vieira <f.vieira06@gmail.com>
2012-10-21 13:52:03 -0200

    Schema e datasource jdbc no projeto do glassfish

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,1 +1,7 @@
-create database monitor;
+CREATE database monitor;
+
+GRANT ALL PRIVILEGES ON monitor.* TO 'efwdbusr'@'localhost' IDENTIFIED BY '4c4b@Tcc';
+
+GRANT ALL PRIVILEGES ON monitor.* TO 'efwdbusr'@'%' IDENTIFIED BY '4c4b@Tcc';
+
+FLUSH PRIVILEGES;
